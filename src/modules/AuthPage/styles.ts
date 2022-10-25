6976b196627b4dc723dc5abf5a47f7bd2f81404e
Tee <teetat.wis@gmail.com>
2022-10-25 13:51:22 +0700

    add facebook-auth

---
+++ src/modules/AuthPage/styles.ts
@@ -0,0 +1,12 @@
+import { createStyles } from "@mantine/core";
+
+export const useStyles = createStyles((theme) => ({
+  brandButton: {
+    marginTop: "30px",
+    marginLeft: "auto",
+    marginRight: "auto",
+    cursor: "pointer",
+  },
+  facebookButton: {},
+  googleButton: {},
+}));
